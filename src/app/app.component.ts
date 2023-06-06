dcaddceb79d4eda3622bbe5515337716e4fae780
Pentakilj97 <127298119+Pentakilj97@users.noreply.github.com>
2023-06-06 12:27:17 +0200

    animations final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('showHide', [
+      state('show', style({
+        left: '500px',
+        opacity: 1,
+      })),
+      state('hide', style({
+        left: '-300px',
+        opacity: 0,
+      })),
+      transition('show => hide', [
+        animate('1s')
+      ]),
+      transition('hide => show', [
+        animate('1s')
+      ]),
+    ])
+  ],
+
 })
 export class AppComponent {
   title = 'angular-animations';
+  isShow = true;
+
+  showHide() {
+    this.isShow = !this.isShow;
+  }
 }
